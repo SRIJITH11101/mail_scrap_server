3b421b0b57e84c3db09d50a9e93e555565b9dd3c
SRIJITH11101 <dovetonsrijith29@gmail.com>
2024-10-05 02:07:33 +0530

    env2 commit

--- mailScraping.py
+++ mailScraping.py
@@ -103,4 +103,4 @@ def run_script():
     return "Data updated successfully", 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
